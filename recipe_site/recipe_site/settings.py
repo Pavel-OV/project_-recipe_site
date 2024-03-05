541075048bc821d7a6509091ac287e03582fba6e
Pavel <black_yaguar@mail.ru>
2024-03-05 14:04:23 +0500

    В файле settings добавлен код в INSTALLED_APPS, TEMPLATES, STATIC_DIRS, import os

--- recipe_site/recipe_site/settings.py
+++ recipe_site/recipe_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipe_site_app',
 ]
 
 MIDDLEWARE = [
@@ -53,8 +55,9 @@ ROOT_URLCONF = 'recipe_site.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS = [
+    os.path.join(BASE_DIR,'static' )
 
-# Default primary key field type
+
+]
+# Default primary key field type'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
