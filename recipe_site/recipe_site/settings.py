a5e40d87de2a0e20f0836efff988ec98fd237f55
Pavel <black_yaguar@mail.ru>
2024-03-14 01:24:14 +0500

    Русифицирована панель администратора, для авториции в INSTALLED_APPS прописали users.apps.UsersConfig

--- recipe_site/recipe_site/settings.py
+++ recipe_site/recipe_site/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipe_site_app',
-    'users',
+    "users.apps.UsersConfig",
     
 ]
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
