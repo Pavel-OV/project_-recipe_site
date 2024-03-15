05e0bb3947e97b23ad82bc470510f6c4d5cf25fe
Pavel <black_yaguar@mail.ru>
2024-03-15 10:34:29 +0500

    Переработан путь для авторизации, поменял на стандартные шаблоны классов поставляемых фреймворком Django

--- recipe_site/users/urls.py
+++ recipe_site/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from users import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
