7b419404b064f03c813cbe6f79cf7541fd18b8c4
Pavel <black_yaguar@mail.ru>
2024-03-15 10:55:00 +0500

    Заменил метод авторизации с функции login_user на стандартные классы, поставляемых фреймворком Django.

--- recipe_site/users/views.py
+++ recipe_site/users/views.py
@@ -1,26 +1,31 @@
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
-from django.contrib.auth import authenticate,  login, logout
-from users.forms import  LoginUserForm
+from django.contrib.auth.views import LoginView
+from multiprocessing import AuthenticationError
+from django.urls import  reverse_lazy
+
+class LoginUser(LoginView):
+    form = AuthenticationError
+    template_name = 'users/login.html'
+    extra_context = {'title': "Авторизация"}
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_site_app:index')
+
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(
+#                 request, username=cd['username'], password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return HttpResponseRedirect(reverse('recipe_site_app:index'))
+#     else:
+#         form = LoginUserForm()
+#     return render(request, 'users/login.html', {'form': form})
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginUserForm()
-    return render(request, 'users/login.html', {'form': form})
-
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('users:login'))
+# def logout_user(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse('recipe_site_app:index'))
