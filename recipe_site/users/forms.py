fbdb486e8134ff50e94f99674a299dacfd374d78
Pavel <black_yaguar@mail.ru>
2024-03-19 18:46:53 +0500

    Создан шаблон для авторизации пользователя

---
+++ recipe_site/users/forms.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from multiprocessing import AuthenticationError
+
+from django import forms
+
+
+class LoginFormUser(AuthenticationForm):
+    username = forms.CharField(
+        max_length=150,
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите имя пользователя'
+        })
+    )
+    password = forms.CharField(
+        max_length=128,
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
