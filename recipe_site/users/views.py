63ea34f67bafb285a42de3e549f57073c54d475e
Pavel <black_yaguar@mail.ru>
2024-03-19 18:44:00 +0500

    Создано представление views  для функции  MyLoginView, которая будет обрабатывать запрос на страницу.

--- recipe_site/users/views.py
+++ recipe_site/users/views.py
@@ -1,3 +1,19 @@
+from multiprocessing import AuthenticationError
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from users.forms import LoginFormUser
+
+class MyLoginView(LoginView):
+    authentication_form = LoginFormUser
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация на сайте'}
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_site_app:index')
+    
+class LoginUser(LoginView):
+    form_class = AuthenticationError
+    template_name = 'users/login.html'
+    extra_context = {'title': "Авторизация"}
