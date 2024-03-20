b01397fd3bc694219e0e998d3226a46fcfd991fa
Pavel <black_yaguar@mail.ru>
2024-03-20 10:56:10 +0500

    Создан путь для формы регистрации рецепта

--- recipe_site/recipe_site_app/urls.py
+++ recipe_site/recipe_site_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipe_site_app'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new_recipe/', views.new_recipe, name='new_recipe'),
 ]
