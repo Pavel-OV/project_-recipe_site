ff1c8a2415f79faf7d11e9a29801ce48d1cf1c2a
Pavel <black_yaguar@mail.ru>
2024-03-21 09:13:20 +0500

    Прописаны пути к all_user_recipes, def all_recipes(request): для взаимодействия с HTML страницой index

--- recipe_site/recipe_site_app/urls.py
+++ recipe_site/recipe_site_app/urls.py
@@ -6,4 +6,8 @@ app_name = 'recipe_site_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('new_recipe/', views.new_recipe, name='new_recipe'),
+    path('all_recipes/', views.all_recipes, name='all_recipes'),
+    path('all_user_recipes/<int:author_id>', views.all_user_recipes, name='all_user_recipes'),
+
+    
 ]
