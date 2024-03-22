4facddabca3fd6277f848fa15f586e97e83e933f
Pavel <black_yaguar@mail.ru>
2024-03-22 08:31:53 +0500

    Добавлены пути к представления(вьюшкам) all_user_recipes, update_recipe, user_recipe,

--- recipe_site/recipe_site_app/urls.py
+++ recipe_site/recipe_site_app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('new_recipe/', views.new_recipe, name='new_recipe'),
     path('all_recipes/', views.all_recipes, name='all_recipes'),
-    path('all_user_recipes/<int:author_id>', views.all_user_recipes, name='all_user_recipes'),
-
+    path('all_user_recipes/<int:author_id>/', views.all_user_recipes, name='all_user_recipes'),
+    path('update_recipe/<int:recipe_id>/', views.update_recipe, name='update_recipe'),
+    path('user_recipe/<int:recipe_id>', views.user_recipe, name='user_recipe'),
     
 ]
