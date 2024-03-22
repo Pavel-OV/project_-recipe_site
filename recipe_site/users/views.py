e43d31bf4e09d7a9c2dabe433a16f3e5c6c58f17
Pavel <black_yaguar@mail.ru>
2024-03-22 08:44:38 +0500

    Добавлен путь к представлению(вьюшк) RegistrationView.as_view()

--- recipe_site/users/views.py
+++ recipe_site/users/views.py
@@ -1,9 +1,10 @@
 from multiprocessing import AuthenticationError
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.models import Group
 
-from users.forms import LoginFormUser
+from users.forms import LoginFormUser, RegistrationForm
 
 class MyLoginView(LoginView):
     authentication_form = LoginFormUser
@@ -12,8 +13,23 @@ class MyLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('recipe_site_app:index')
-    
+
+class RegistrationView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'users/registration.html'
+    extra_context = {'title': 'Регистрация на сайте'}
+
+    def get_success_url(self):
+        return reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        group = Group.objects.get(name='Кулинары')
+        user.groups.add(group)
+        return super().form_valid(form)
+
 class LoginUser(LoginView):
     form_class = AuthenticationError
     template_name = 'users/login.html'
     extra_context = {'title': "Авторизация"}
+
