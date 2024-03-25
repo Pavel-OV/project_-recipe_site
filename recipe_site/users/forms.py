69698e54a2030af11e80183155d88a60a9c47f33
Pavel <black_yaguar@mail.ru>
2024-03-25 16:19:05 +0500

    Приложение user: создан путь для регистрации пользователя registration, создана минимальная форма регистрации нового пользователя

--- recipe_site/users/forms.py
+++ recipe_site/users/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
-from multiprocessing import AuthenticationError
+from django.contrib.auth.forms import UserCreationForm
 
 from django import forms
 
@@ -26,4 +26,40 @@ class LoginFormUser(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите имя пользователя'
+        })
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите email'
+        })
+    )
+    password1 = forms.CharField(
+        max_length=128,
+        label='Пароль',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+    )
+    password2 = forms.CharField(
+        max_length=128,
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Повторите пароль'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', ]
 
--- recipe_site/users/urls.py
+++ recipe_site/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.MyLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
 ]
