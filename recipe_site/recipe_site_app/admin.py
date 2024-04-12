59155d58a70723136b31a6b62c16d7fadfd10301
Pavel <black_yaguar@mail.ru>
2024-04-12 13:15:59 +0500

    Расширил административную панель - реценты

--- recipe_site/recipe_site_app/admin.py
+++ recipe_site/recipe_site_app/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 from .models import Recipes, Category
-from .models import *
 
-admin.site.register(Recipes)
-admin.site.register(Category)
+
+@admin.register(Recipes)
+class RecipesModelAdmin(admin.ModelAdmin):
+    list_display = ['title_recipe', 'author','cooking_time', 'views','date_of_publications']
+    readonly_fields = ['date_of_publications',]
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title_recipe',"description",],
+            },
+        ),
+        (
+            
+            'Полная информация о рецепте',
+            
+            {
+                
+                'classes': ['collapse'],
+                'description': 'Информация о рецепте',
+                'fields': ["ingredients",'cooking_steps','cooking_time',],
+            },
+        ),
+    ]
+
+@admin.register(Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['title_category',]
